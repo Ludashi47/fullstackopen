0484572c2b326a9dd3e8ae40d682eaf0212830ea
Jiaxuan Lu <jiaxuan@Jiaxuans-MacBook-Air.local>
2026-01-10 14:47:21 +0200

    Exercise 1.5

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,55 +1,57 @@
 const App = () => {
-  const course = 'Half Stack application development'
 
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
-
-  const Header = ({ course }) => {
-    return (
-      <h1>{course}</h1>
-    )
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
   }
 
-  const Content = ({ parts }) => {
-    return (
-      parts.map(
-        part =>
-          <p>{part.name} {part.exercises}</p>
-      )
+const Header = ({ course }) => {
+  return (
+    <h1>{course.name}</h1>
+  )
+}
+
+const Content = ({ course }) => {
+  return (
+    course.parts.map(
+      part =>
+        <p>{part.name} {part.exercises}</p>
     )
-  }
+  )
+}
 
-  const Total = ({parts }) => {
-    let sum = 0
+const Total = ({ course }) => {
+  let sum = 0
 
-    for (let part of parts) {
-      sum += part.exercises
-    }
-
-    return (
-      <p>Number of exercises {sum}</p>
-    )
+  for (let part of course.parts) {
+    sum += part.exercises
   }
 
   return (
-    <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
-    </div>
+    <p>Number of exercises {sum}</p>
   )
+}
+
+return (
+  <div>
+    <Header course={course} />
+    <Content course={course} />
+    <Total course={course} />
+  </div>
+)
 
 
 }
